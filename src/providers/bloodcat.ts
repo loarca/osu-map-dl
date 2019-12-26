6f961b13c2e311d293664f1e947f23608dc66b97
Alejandro Loarca <ale.j.loarca@gmail.com>
2019-12-25 22:41:16 -0600

    Use axios instances instead of global object

--- src/providers/bloodcat.ts
+++ src/providers/bloodcat.ts
@@ -1,10 +1,20 @@
-import axios, { AxiosResponse } from 'axios'
+import axios, { AxiosResponse, AxiosInstance } from 'axios'
 import { Readable } from 'stream'
 import { BaseProvider } from './base'
 
 export class BloodcatProvider extends BaseProvider {
+  private axios: AxiosInstance
+
+  constructor () {
+    super()
+    this.axios = axios.create({
+      baseURL: 'https://bloodcat.com/osu/s/',
+      withCredentials: false
+    })
+  }
+
   protected implementation (beatmapsetID: number): Promise<AxiosResponse<Readable>> {
-    return axios.get(`https://bloodcat.com/osu/s/${beatmapsetID}`, {
+    return this.axios.get(`/${beatmapsetID}`, {
       responseType: 'stream'
     })
   }
