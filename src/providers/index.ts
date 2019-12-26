32cb979c38c520bf02333345a657e61ccf53843f
Alejandro Loarca <ale.j.loarca@gmail.com>
2019-12-25 22:47:34 -0600

    Fix #10

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,1 +1,7 @@
+import axios from 'axios'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { name, version } = require('../../package.json')
+
+axios.defaults.headers['User-Agent'] = `${name}/${version}`
+
 export * from './bloodcat'
